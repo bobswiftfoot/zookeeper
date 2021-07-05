bcbc278ed74dc6f7f5dffa14d8d7597d2483483c
Nate Pfau <natepfau@yahoo.com>
2021-07-05 16:21:38 -0500

    Added get by Id

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ app.get('/api/animals', (req, res) =>
     res.json(results);
 });
 
+app.get('/api/animals/:id', (req, res) =>
+{
+    const result = findById(req.params.id, animals);
+    if (result)
+    {
+        res.json(result);
+    }
+    else
+    {
+        res.send(404);
+    }
+});
+
 function filterByQuery(query, animalsArray)
 {
     let personalityTraitsArray = [];
@@ -30,7 +43,7 @@ function filterByQuery(query, animalsArray)
         if (typeof query.personalityTraits === 'string')
         {
             personalityTraitsArray = [query.personalityTraits];
-        } 
+        }
         else
         {
             personalityTraitsArray = query.personalityTraits;
@@ -64,4 +77,10 @@ function filterByQuery(query, animalsArray)
     }
     // return the filtered results:
     return filteredResults;
+}
+
+function findById(id, animalsArray)
+{
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
